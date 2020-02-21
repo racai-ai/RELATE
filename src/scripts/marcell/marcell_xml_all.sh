ff7619f1eeb44fa87e37471e34a510537eb9f3ae
pvf2005 <pvf2005@gmail.com>
2020-02-21 14:30:18 +0200

    Marcell export fixes

--- src/scripts/marcell/marcell_xml_all.sh
+++ src/scripts/marcell/marcell_xml_all.sh
@@ -8,7 +8,9 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 
 fi
 
-source $1/bin/activate
-mkdir -p $2/marcell-xml
+source "$1/bin/activate"
+rm -fr "$2/marcell-xml"
+mkdir -p "$2/marcell-xml"
 
-find $2/basic_tagging -name '*conllup' | parallel --citation -j 20 "python conllup2xml.py < {} > $2/ marcell-xml/\$(basename {}).xml"
+
+find "$2/basic_tagging" -name '*conllup' | parallel -j 20 "python conllup2xml.py < {} > \"$2/marcell-xml/\$(basename {}).xml\""
