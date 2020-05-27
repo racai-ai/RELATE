7eaedd7b3a7b0d39ab76fd407ae9b6404e6d5164
pvf2005 <pvf2005@gmail.com>
2020-05-27 14:48:26 +0300

    updated dev doc link

--- src/pages/teprolin/doc_dev.php
+++ src/pages/teprolin/doc_dev.php
@@ -2,7 +2,7 @@
 
 function getPageContent(){
     $html=file_get_contents(realpath(dirname(__FILE__))."/../iframe_common.html");
-    $html=str_replace("{{src}}","http://89.38.230.23:5000/",$html);
+    $html=str_replace("{{src}}","http://relate.racai.ro:5000/",$html);
     return $html;
 }
 
