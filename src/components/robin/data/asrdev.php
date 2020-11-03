114dda078c6509e61472cf84ff5d642ef152e2b7
pvf2005 <pvf2005@gmail.com>
2020-11-03 11:35:20 +0200

    ASR dev integration

---
+++ src/components/robin/data/asrdev.php
@@ -0,0 +1,18 @@
+<?php
+
+if(!isset($_FILES['asrfile']))
+  die("Invalid usage");
+
+$fname=$_FILES['asrfile']['tmp_name'];
+$d=WAV_getDuration($fname);
+if($d===false || $d>5*60){
+    echo json_encode(["asr"=>"","error"=>true,"success"=>""]);
+    die();
+}
+
+$fileContent = file_get_contents($fname);
+$result=ROBIN_runASR($fileContent,true);
+//echo json_encode(["asr"=>$result['data'],"error"=>false,"success"=>$result['success']]);
+echo json_encode(["asr"=>$result['transcription'],"error"=>false,"success"=>$result['status']]);
+
+?>
