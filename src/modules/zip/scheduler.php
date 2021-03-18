9f9de5d7490aee905a005cf2656ca781e0adcc67
pvf2005 <59016899+pvf2005@users.noreply.github.com>
2021-03-18 14:48:48 +0200

    Additional ZIP tasks for standoff and gold annotations (#17)
    * Additional ZIP tasks for standoff and gold annotations
    
    * allow archive to be downloaded
    
    Co-authored-by: vasile <vasile@localhost>

--- src/modules/zip/scheduler.php
+++ src/modules/zip/scheduler.php
@@ -18,4 +18,16 @@ function scheduleUnzipAnnotated($settings,$corpus,$task_name,$tdata){
     scheduleFile($corpus,"zip_".$settings->get("dir.annotated")."/".$tdata['fname'],$task_name,'zip');
 }
 
+function scheduleZipStandoff($settings,$corpus,$task_name,$tdata){
+    scheduleFile($corpus,"zip_standoff/".$tdata['fname'],$task_name,'zip');
+}
+
+function scheduleZipGoldStandoff($settings,$corpus,$task_name,$tdata){
+    scheduleFile($corpus,"zip_gold_standoff/".$tdata['fname'],$task_name,'zip');
+}
+
+function scheduleZipGoldAnn($settings,$corpus,$task_name,$tdata){
+    scheduleFile($corpus,"zip_gold_ann/".$tdata['fname'],$task_name,'zip');
+}
+
 ?>
