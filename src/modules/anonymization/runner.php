6bb571386cca9720fbf82a4c6a4fa4d6bc53cda7
pvf2005 <59016899+pvf2005@users.noreply.github.com>
2021-04-01 15:16:17 +0300

    Anonimization module (#18)
    * module for text anonymization
    
    * correction
    
    * correction
    
    * correction
    
    Co-authored-by: vasile <vasile@localhost>

---
+++ src/modules/anonymization/runner.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\anonymization;
+
+function anonymizeFileContent($fcontent,$fpathOut){
+    file_put_contents($fpathOut,ANONYMIZATION_anonymize_text($fcontent));
+}
+
+function runner($runner,$settings,$corpus,$taskDesc,$data,$contentIn,$fnameOut){
+    $path=$corpus->getFolderPath()."/files/";
+    $fnameOut=changeFileExtension($fnameOut,"txt");
+    $finalFile=$path.$fnameOut;
+    echo "Destination for anonymization $finalFile\n";
+    @mkdir($path);        
+
+    anonymizeFileContent($contentIn,$finalFile);
+    
+    file_put_contents($corpus->getFolderPath()."/changed_files.json",json_encode(["changed"=>time()]));            
+}
+
+
+?>
---
+++ src/modules/anonymization/scheduler.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\anonymization;
+
+function schedule($settings,$corpus,$task_name,$tdata){
+   scheduleFilesFolder($corpus,$task_name);
+}
+
+?>
