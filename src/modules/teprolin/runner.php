5b5561a7827f6a5f71027a978d21b7cb1f7e34d8
vasile <vasile@localhost>
2021-05-15 07:12:11 +0300

    Allows selection of NER type

--- src/modules/teprolin/runner.php
+++ src/modules/teprolin/runner.php
@@ -21,12 +21,25 @@ function runner($runner,$settings,$corpus,$taskDesc,$data,$contentIn,$fnameOut){
     
     @mkdir($path);    
     
-    runBasicTaggingText_ro($contentIn,$finalFile,$runner->getRunnerId());
+    $allowedNER=[];
+    if(isset($taskDesc['allowed_ner'])){
+        $allowedNER=explode(",",$taskDesc['allowed_ner']);
+        if(count($allowedNER)>0)$allowedNER=array_flip($allowedNER);
+    }
+    
+    $useBIONER=false;
+    $useNER=false;
+    $stripBI=false;
+    if(isset($taskDesc['use_bioner']) && strcasecmp($taskDesc['use_bioner'],"YES")==0)$useBIONER=true;
+    if(isset($taskDesc['use_ner']) && strcasecmp($taskDesc['use_ner'],"YES")==0)$useNER=true;
+    if(isset($taskDesc['strip_bi']) && strcasecmp($taskDesc['strip_bi'],"YES")==0)$stripBI=true;
+    
+    runBasicTaggingText_ro($contentIn,$finalFile,$runner->getRunnerId(),$allowedNER,$useNER,$useBIONER,$stripBI);
     
     file_put_contents($corpus->getFolderPath()."/changed_basictagging.json",json_encode(["changed"=>time()]));            
 }
 
-function runBasicTaggingText_ro($text,$fout,$trun){
+function runBasicTaggingText_ro($text,$fout,$trun,$allowedNER,$useNER,$useBIONER,$stripBI){
     
     //$text=preg_replace("/[^ a-zA-Z0-9.,;_+%&\$#()\\[\\]ăîâșțĂÎÂȘȚ]/u"," ",$text);
     //$text=preg_replace("/[ ]+/"," ",$text);
@@ -58,14 +71,18 @@ function runBasicTaggingText_ro($text,$fout,$trun){
 						                            
 						                    	    if(!isset($tok['ner'])){
 								                        	$tok['ner']="O";
-								                        	if(isset($tok['_ner']) && strlen($tok['_ner'])>0)
+								                        	if(isset($tok['_ner']) && strlen($tok['_ner'])>0 && $useNER)
 								                            	    $tok['ner']=$tok['_ner'];
-								                        	if(isset($tok['_bner']) && strlen($tok['_bner'])>0){
-								                            	    $tok['ner']="O";//$tok['_bner'];
+								                        	if(isset($tok['_bner']) && strlen($tok['_bner'])>0 && $useBIONER){
+								                            	    $tok['ner']=$tok['_bner']; // "O"
 								                                // skip for now BIONER
 								                                /*if(startsWith($tok['ner'],"I-") || startsWith($tok['ner'],"B-"))
 								                                    $tok['ner']=substr($tok['ner'],2);*/
 								                        	}
+								                            if($stripBI && startsWith($tok['ner'],"I-") || startsWith($tok['ner'],"B-"))
+								                                    $tok['ner']=substr($tok['ner'],2);
+                                                            if(count($allowedNER)>0 && !isset($allowedNER[$tok['ner']]))
+                                                                    $tok['ner']='O';
 						                    	    }
 						                	}
 			            	    }
