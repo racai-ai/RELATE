e753ead4fd621f34a7b4c2675a4eb6a7bb774753
vasile <vasile@localhost>
2021-05-19 15:38:31 +0300

    file ownership

--- src/modules/zip/runner.php
+++ src/modules/zip/runner.php
@@ -14,20 +14,29 @@ function runZip($pathIn,$pathOut,$fnameOut){
 function runUnzip($fnameIn,$pathOut,$settings,$corpus,$taskDesc){
     
     @mkdir($pathOut);    
-
+    @chown($pathOut,$settings->get("owner_user"));
+    @chgrp($pathOut,$settings->get("owner_group"));
+    
     passthru("unzip -j -o ".escapeshellarg($fnameIn)." -d ".escapeshellarg($pathOut));
+    passthru("chown -R ".$settings->get("owner_user").":".$settings->get("owner_group")." ".escapeshellarg($pathOut));
    
     $dir_meta=$corpus->getFolderPath();
     $dir_meta.="/meta";
     @mkdir($dir_meta);
-
+    @chown($dir_meta,$settings->get("owner_user"));
+    @chgrp($dir_meta,$settings->get("owner_group"));
+    
     $dir_standoff=$corpus->getFolderPath();
     $dir_standoff.="/standoff";
     @mkdir($dir_standoff);
+    @chown($dir_standoff,$settings->get("owner_user"));
+    @chgrp($dir_standoff,$settings->get("owner_group"));    
 
     $dir_annotated=$corpus->getFolderPath();
     $dir_annotated.="/".$settings->get("dir.annotated");
     @mkdir($dir_annotated);
+    @chown($dir_annotated,$settings->get("owner_user"));
+    @chgrp($dir_annotated,$settings->get("owner_group"));    
 
     $dh = opendir($pathOut);
     while (($file = readdir($dh)) !== false) {
@@ -40,7 +49,8 @@ function runUnzip($fnameIn,$pathOut,$settings,$corpus,$taskDesc){
         
         if(endsWith(strtolower($file),".txt")){
             if(!is_file($pathMeta)){
-                file_put_contents($dir_meta."/".$file.".meta",json_encode([
+                $fpathMeta=$dir_meta."/".$file.".meta";
+                file_put_contents($fpathMeta,json_encode([
                     'name' => $file,
                     'corpus' => $corpus->getData("name","unknown"),
                     'type' => 'text',
@@ -48,12 +58,19 @@ function runUnzip($fnameIn,$pathOut,$settings,$corpus,$taskDesc){
                     'created_by' => $taskDesc['created_by'],
                     'created_date' => $taskDesc['created_date']
                 ]));
+                @chown($fpathMeta,$settings->get("owner_user"));
+                @chgrp($fpathMeta,$settings->get("owner_group"));
                 
             }
         }else if(endsWith(strtolower($file),".conllu") || endsWith(strtolower($file),".conllup")){
             @rename($pathFile,$pathAnnotated);
+            @chown($pathAnnotated,$settings->get("owner_user"));
+            @chgrp($pathAnnotated,$settings->get("owner_group"));
+            
         }else{
             @rename($pathFile,$pathStandoff);
+            @chown($fpathStandoff,$settings->get("owner_user"));
+            @chgrp($fpathStandoff,$settings->get("owner_group"));
         }
     }
     closedir($dh);
@@ -61,17 +78,30 @@ function runUnzip($fnameIn,$pathOut,$settings,$corpus,$taskDesc){
     file_put_contents($corpus->getFolderPath()."/changed_files.json",json_encode(["changed"=>time()]));            
     file_put_contents($corpus->getFolderPath()."/changed_standoff.json",json_encode(["changed"=>time()]));            
     file_put_contents($corpus->getFolderPath()."/changed_annotated.json",json_encode(["changed"=>time()]));            
-    
+
+    @chown($corpus->getFolderPath()."/changed_files.json",$settings->get("owner_user"));
+    @chgrp($corpus->getFolderPath()."/changed_files.json",$settings->get("owner_group"));
+        
+    @chown($corpus->getFolderPath()."/changed_standoff.json",$settings->get("owner_user"));
+    @chgrp($corpus->getFolderPath()."/changed_standoff.json",$settings->get("owner_group"));
+
+    @chown($corpus->getFolderPath()."/changed_annotated.json",$settings->get("owner_user"));
+    @chgrp($corpus->getFolderPath()."/changed_annotated.json",$settings->get("owner_group"));
 }
 
 function runUnzipAnnotated($fnameIn,$pathOut){
     global $runnerFolder,$corpus,$settings,$taskDesc;
     
     @mkdir($pathOut);    
+    @chown($pathOut,$settings->get("owner_user"));
+    @chgrp($pathOut,$settings->get("owner_group"));
 
     passthru("unzip -j -o ".escapeshellarg($fnameIn)." -d ".escapeshellarg($pathOut));
+    passthru("chown -R ".$settings->get("owner_user").":".$settings->get("owner_group")." ".escapeshellarg($pathOut));
    
     file_put_contents($corpus->getFolderPath()."/changed_annotated.json",json_encode(["changed"=>time()]));            
+    @chown($corpus->getFolderPath()."/changed_annotated.json",$settings->get("owner_user"));
+    @chgrp($corpus->getFolderPath()."/changed_annotated.json",$settings->get("owner_group"));
 }
 
 function runnerZipText($runner,$settings,$corpus,$taskDesc,$data,$contentIn,$fnameOut){
