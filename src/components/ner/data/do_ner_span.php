ecec26595298b7491964cf5b891c3d2ec03e7ae1
vasile <vasile@localhost>
2021-08-13 13:30:28 +0300

    update ner models

--- src/components/ner/data/do_ner_span.php
+++ src/components/ner/data/do_ner_span.php
@@ -6,6 +6,8 @@ if(!isset($_REQUEST['model']))die("Invalid call");
 $models=[
 "legalnero_legal_per_loc_org_time" => ["url"=>"http://127.0.0.1:5101/api/v1.0/ner"],
 "legalnero_per_loc_org_time" => ["url"=>"http://127.0.0.1:5102/api/v1.0/ner"],
+"legalnero_legal_per_loc_org_time_gaz" => ["url"=>"http://127.0.0.1:5103/api/v1.0/ner"],
+"legalnero_per_loc_org_time_gaz" => ["url"=>"http://127.0.0.1:5104/api/v1.0/ner"],
 ];
 
 $text=$_REQUEST['text'];
