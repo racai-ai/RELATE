bd29dfd6ca8231de044558b1e686f062ba5ced4a
vasile <vasile@localhost>
2021-09-13 12:28:32 +0300

    use POST for anonymization

--- src/lib/anonymization.php
+++ src/lib/anonymization.php
@@ -2,7 +2,9 @@
 
 function ANONYMIZATION_anonymize_text($text){
 
-    $out=[];
+    $url="http://127.0.0.1:8202/anonymize";
+
+    /*$out=[];
     foreach(explode("\n",$text) as $line){
             $l=trim($line);
             if(empty($l))$out[]=$line;
@@ -14,7 +16,45 @@ function ANONYMIZATION_anonymize_text($text){
             }
     }
     
-    return implode("\n",$out);
+    return implode("\n",$out);*/
+    
+    $ch = curl_init();
+    
+    $boundary = uniqid();
+    $delimiter = '-------------' . $boundary;
+    $eol = "\r\n";
+    $data="";
+    $data .= "--" . $delimiter . $eol
+                . 'Content-Disposition: form-data; name="text"; filename="text.txt"'.$eol
+                . 'Content-type: text/text'.$eol.$eol
+                . $text . $eol;
+    $data .= "--" . $delimiter . "--".$eol;
 
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 60,
+        //CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => array(
+          //"Authorization: Bearer $TOKEN",
+          "Content-Type: multipart/form-data; boundary=" . $delimiter,
+          "Content-Length: " . strlen($data)
+      
+        ),
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        //CURLOPT_VERBOSE => $ROBIN_DEBUG
+    ));
+    
+    
+    $server_output = curl_exec($ch);
+    
+    curl_close ($ch);
+    
+    return $server_output;
 
 }
