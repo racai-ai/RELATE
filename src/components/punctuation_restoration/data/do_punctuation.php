baca5b9c9a0577ad227b991e044745f418f93e51
pvf2005 <59016899+pvf2005@users.noreply.github.com>
2021-10-04 14:20:19 +0300

    Feature/punctuation restoration (#27)
    * Romanian punctuation restoration
    
    * Romanian punctuation restoration
    
    * Romanian punctuation restoration
    
    * Romanian punctuation restoration
    
    Co-authored-by: vasile <vasile@localhost>

---
+++ src/components/punctuation_restoration/data/do_punctuation.php
@@ -0,0 +1,80 @@
+<?php
+
+if(!isset($_REQUEST['text']))die("Invalid call");
+if(!isset($_REQUEST['model']))die("Invalid call");
+
+$models=[
+"marcell_punctuation" => ["url"=>"http://127.0.0.1:5105/api/v1.0/punctuation"],
+];
+
+$text=$_REQUEST['text'];
+$model=$_REQUEST['model'];
+if(!isset($models[$model]))die("Invalid call");
+
+
+function callPunctuation($url,$text){
+    $ch = curl_init();
+    
+    $boundary = uniqid();
+    $delimiter = '-------------' . $boundary;
+    $eol = "\r\n";
+    $data="";
+    $data .= "--" . $delimiter . $eol
+                . 'Content-Disposition: form-data; name="text"; filename="text.txt"'.$eol
+                . 'Content-type: text/text'.$eol.$eol
+                . implode("\n",explode(" ",$text)) . $eol;
+    $data .= "--" . $delimiter . "--".$eol;
+
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 60,
+        //CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => array(
+          //"Authorization: Bearer $TOKEN",
+          "Content-Type: multipart/form-data; boundary=" . $delimiter,
+          "Content-Length: " . strlen($data)
+      
+        ),
+        CURLOPT_SSL_VERIFYHOST => 0,
+        CURLOPT_SSL_VERIFYPEER => 0,
+        //CURLOPT_VERBOSE => $ROBIN_DEBUG
+    ));
+    
+    
+    $server_output = curl_exec($ch);
+    
+    curl_close ($ch);
+
+    return $server_output;
+}
+
+
+
+
+$url=$models[$model]["url"];
+
+$data=callPunctuation($url,$text);
+//$data='{"text":"Acesta este un test.","mappings":["aaaa\\t_#312312#"]}';
+
+$data=json_decode($data,true);
+$newt="";
+$rdata=explode(" ",$data['result']);
+$tdata=explode(" ",$text);
+$ndata=[];
+for($i=0;$i<count($tdata);$i++){
+    $ndata[]=$tdata[$i];
+    if($rdata[$i]=="COMMA")$ndata[]=",";
+    if($rdata[$i]=="PERIOD")$ndata[]=".";
+}
+
+if($ndata[count($ndata)-1]!=".")$ndata[]=".";
+
+$data['new_text']=implode(" ",$ndata);
+echo json_encode($data);
+
+die();
