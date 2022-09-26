b5701f0dcdf27739d6034ba4ca40c922dc0236ff
vasile <vasile@localhost>
2022-09-26 14:37:51 +0300

    run ner on paragraphs

--- src/modules/ner_legalner/runner.php
+++ src/modules/ner_legalner/runner.php
@@ -3,14 +3,34 @@
 namespace Modules\ner_legalner;
 
 function runNERL($fcontent,$fpathOut,$url){
-    $data=NER_callNER($url,$fcontent);
-    $data=json_decode($data,true);
     $rdata=[];
-    if($data!==null && is_array($data) && isset($data['status']) && $data['status']=="OK"){
-        foreach($data['result'] as $ob){
-            $rdata[]="${ob['id']}\t${ob['type']} ${ob['start']} ${ob['end']}\t${ob['text']}";
+    
+    $last_pos=0;
+    $id=1;
+    while(true){
+        $pos=strpos($fcontent,"\n",$last_pos);
+        if($pos===false)$current=substr($fcontent,$last_pos);
+        else{
+            $current=substr($fcontent,$last_pos,$pos-$last_pos);
         }
-
+        
+        if(strlen(trim($current))>0){    
+            $data=NER_callNER($url,$current);
+            $data=json_decode($data,true);
+            if($data!==null && is_array($data) && isset($data['status']) && $data['status']=="OK"){
+                foreach($data['result'] as $ob){
+                    $ob['start']=intval($ob['start'])+$last_pos;
+                    $ob['end']=intval($ob['end'])+$last_pos;
+                    $ob['id']="T".$id;
+                    $id++;
+                    $rdata[]="${ob['id']}\t${ob['type']} ${ob['start']} ${ob['end']}\t${ob['text']}";
+                }
+        
+            }
+        }
+     
+        if($pos===false)break;   
+        $last_pos=$pos+1;
     }
     file_put_contents($fpathOut,implode("\n",$rdata));
 }
